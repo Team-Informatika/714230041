b2e83cee926b000a4b9ee907a04e9a9f3da2fdc7
Muhamad Adi Apriansyah <147012357+owel17@users.noreply.github.com>
2025-05-08 14:48:41 +0700

    p

--- BELAJARgolang/main.go
+++ BELAJARgolang/main.go
@@ -9,7 +9,7 @@ func main() {
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, disuruh bagus!")
+		return c.SendString("mantap!")
 	})
 
 	log.Fatal(app.Listen(":3000"))
