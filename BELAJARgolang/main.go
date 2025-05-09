1b1ab1884938273036d767f8c1bc7a31e631a9a8
Muhamad Adi Apriansyah <147012357+owel17@users.noreply.github.com>
2025-05-09 11:27:39 +0700

    p

--- BELAJARgolang/main.go
+++ BELAJARgolang/main.go
@@ -9,7 +9,7 @@ func main() {
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("kucing!")
+		return c.SendString("sapi!")
 	})
 
 	log.Fatal(app.Listen(":3000"))
